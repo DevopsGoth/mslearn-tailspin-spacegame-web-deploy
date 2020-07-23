5c67485c7f024f61cdcc925bfa81d5f1410ae985
June Boston <38109440+DevopsGoth@users.noreply.github.com>
2020-07-23 08:25:57 -0600

    site change also update ps script

--- scripts/Install.ps1
+++ scripts/Install.ps1
@@ -1,4 +1,6 @@
-echo "install step here" >> C:\temp\install.log
+echo "installing build info file" >> C:\temp\install.log
 
 expand-archive C:\temp\WebApp\Tailspin.SpaceGame.Web.zip C:\temp\tailspin
-cp C:\temp\tailspin\wwwroot\buildinfo.txt C:\inetpub\wwwroot\
+Copy-Item C:\temp\tailspin\wwwroot\buildinfo.txt C:\inetpub\wwwroot\buildinfo.txt
+
+Get-Content C:\temp\tailspin\wwwroot\buildinfo.txt >> C:\temp\install.log
