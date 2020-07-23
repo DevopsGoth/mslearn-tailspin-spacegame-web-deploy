c3a061f139dcadafc9b9b5062eab81b215c37e37
June Boston <38109440+DevopsGoth@users.noreply.github.com>
2020-07-23 08:31:57 -0600

    put that install code inafterinstall

--- scripts/AfterInstall.ps1
+++ scripts/AfterInstall.ps1
@@ -1,1 +1,7 @@
-Write-Output "done" >> "C:\\temp\\done.log"
+Write-Output "installing build info file" >> C:\temp\install.log
+
+expand-archive "C:\temp\WebApp\Tailspin.SpaceGame.Web.zip" "C:\temp\WebApp"
+Copy-Item C:\temp\WebApp\wwwroot\buildinfo.txt C:\inetpub\wwwroot\
+
+Get-Content C:\temp\tailspin\wwwroot\buildinfo.txt >> C:\temp\install.log
+Write-Output "donee" >> "C:\\temp\\done.log"
